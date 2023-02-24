150a0edccb3c011fccab7cb975f7f1ee62b451fe
Exopteron <exopteron@protonmail.com>
2023-02-25 11:28:13 +1300

    servidiot-primitives: block ID improvements

--- servidiot-primitives/src/block.rs
+++ servidiot-primitives/src/block.rs
@@ -26,6 +26,31 @@ impl BlockID {
         }
     }
 
+    /// Creates a new `BlockID` from 
+    /// the type byte and add byte.
+    /// Returns `None` if `add` is greater than 15.
+    pub const fn new_with_add(block_id: u8, add: u8) -> Option<Self> {
+        if add > 15 {
+            return None;
+        }
+        let mut id = (add as u16) << 8;
+        id += block_id as u16;
+        Some(Self(id))
+    }
+
+    /// Whether this block ID needs Add data
+    /// to represent it.
+    pub const fn needs_add(&self) -> bool {
+        self.0 > 255
+    }
+
+    /// Extracts the ID and add pair out of this ID.
+    pub const fn to_add_pair(&self) -> (u8, u8) {
+        let id = (self.0 & 0xff) as u8;
+        let add = (self.0 & 0xf00) as u8;
+        (id, add)
+    }
+
     /// # Safety
     /// Ensure `block_id` is no larger than 4096.
     pub const unsafe fn new_unchecked(block_id: u16) -> Self {
@@ -36,4 +61,14 @@ impl BlockID {
 
 pub trait BlockType {
     const VALID_BLOCK_TYPES: &'static [BlockID];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BlockID;
+
+    #[test]
+    fn block_id_test() {
+        assert_eq!(256, BlockID::new_with_add(0, 1).unwrap().0);
+    }
 }
